15b962392db1a59976530b7b29c74fe223ec4be3
shahzod <shahzodnematov33@gmail.com>
2022-08-10 09:50:22 +0500

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [weather, setWeather] = useState([{}]);
+  const [city, setCity] = useState("");
+
+  const apiKey = "055cf4e4ce4919f11fde4532a65f80f5";
+
+  const getWeather = (e) => {
+    if (e.key === "Enter") {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setCity("");
+        });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <input
+          type="text"
+          placeholder="Search for a city..."
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          onKeyPress={getWeather}
+        />
+
+        {typeof weather.main != "undefined" ? (
+          <div className="weather-container">
+            <div className="location">
+              <i>&#xf1ad;</i> {weather.name} , {weather.sys.country}
+            </div>
+            <div className="temp">
+              {" "}
+              <i>&#xf185;</i> {Math.round(weather.main.temp)}*C
+            </div>
+            <div className="weather">
+              {" "}
+              <i>&#xf75f;</i> {weather.weather[0].main}
+            </div>
+            <div className="wind">
+              {" "}
+              <i>&#xf72e;</i> {weather.wind.speed} m/s
+            </div>
+          </div>
+        ) : (
+          <div className="weather-container">
+            <h1>Welcome to Weather App</h1>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
